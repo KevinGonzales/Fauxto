8efa53696a1e3d68baacac980d79ce92e05a61f5
Kevin Gonzales <KevinGonzalesVargas@gmail.com>
2018-03-05 17:49:31 -0600

    Initial commit

---
+++ app/src/main/java/ui/com/fauxto/Login/LoginActivity.java
@@ -0,0 +1,15 @@
+package ui.com.fauxto.Login;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import ui.com.fauxto.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
