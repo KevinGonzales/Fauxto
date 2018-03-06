513c959016ef58494f62b0322263430d7cbc9756
Myka Hancevic <bsx745@my.utsa.edu>
2018-03-05 20:32:45 -0600

    added registerpage

--- app/src/main/java/ui/com/fauxto/Login/LoginActivity.java
+++ app/src/main/java/ui/com/fauxto/Login/LoginActivity.java
@@ -2,6 +2,8 @@ package ui.com.fauxto.Login;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import ui.com.fauxto.R;
 
@@ -11,6 +13,15 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        Button loginButton =  (Button) findViewById(R.id.loginButton) ;
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 
 
