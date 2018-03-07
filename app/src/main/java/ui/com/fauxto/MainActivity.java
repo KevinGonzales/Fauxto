575c384053685cfaa139452c8a25b1afca7f908f
Kevin Gonzales <KevinGonzalesVargas@gmail.com>
2018-03-07 14:34:59 -0600

    Bottom navigation bar is working

---
+++ app/src/main/java/ui/com/fauxto/MainActivity.java
@@ -0,0 +1,61 @@
+package ui.com.fauxto;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+import ui.com.fauxto.ProfilePage.ProfilePageController;
+import ui.com.fauxto.UserFeed.UserFeedController;
+
+/**
+ * Created by kevingonzales on 3/7/18.
+ */
+
+public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_app_view);
+
+        BottomNavigationView navigationView = (BottomNavigationView) findViewById(R.id.navigationBar);
+
+        navigationView.setOnNavigationItemSelectedListener(this);
+
+        loadFragment(new UserFeedController());
+    }
+
+    @Override //Whenever we tap on the menu on the navigation bar this method is called
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+        Fragment fragment = null;
+
+        switch (item.getItemId()){
+            case R.id.navigation_feed:
+                fragment = new UserFeedController();
+                break;
+
+            case R.id.navigation_profilePic:
+                fragment = new ProfilePageController();
+                break;
+        }
+
+        return loadFragment(fragment);
+    }
+
+    private boolean loadFragment(Fragment fragment){
+        if(fragment!=null){
+
+            getFragmentManager().beginTransaction().replace(R.id.fragment_container,fragment).commit();
+
+            return  true;
+        }
+        return false;
+    }
+}
