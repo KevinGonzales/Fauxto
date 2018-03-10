f693698c89eda1e43f8948d1733e4acc44925217
Kevin Gonzales <KevinGonzalesVargas@gmail.com>
2018-03-10 02:06:03 -0600

    camera uploads incorectly

--- app/src/main/java/ui/com/fauxto/Camera/CameraActivity.java
+++ app/src/main/java/ui/com/fauxto/Camera/CameraActivity.java
@@ -1,7 +1,8 @@
 package ui.com.fauxto.Camera;
-
 import android.Manifest;
 import android.app.Activity;
+import android.content.ContentResolver;
+import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
@@ -10,40 +11,28 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
+import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
-import android.support.v4.content.FileProvider;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
 import java.io.File;
-import java.io.IOException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-
-
-import android.Manifest;
-import android.app.Activity;
-import android.app.AlertDialog;
-
-import android.content.Context;
-import android.content.DialogInterface;
-import android.content.Intent;
-import android.content.SharedPreferences;
-import android.content.pm.PackageManager;
-
-import android.net.Uri;
-import android.os.Bundle;
-import android.provider.Settings;
-
-import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.ContextCompat;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+import java.util.UUID;
 
 import ui.com.fauxto.R;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 import static android.content.ContentValues.TAG;
 
@@ -51,40 +40,47 @@ import static android.content.ContentValues.TAG;
  * Created by kevingonzales on 3/7/18.
  */
 
-public class CameraActivity extends Activity {
+public class CameraActivity extends AppCompatActivity {
 
-    private static final int CAMERA_REQUEST = 1888;
-    private ImageView imageView;
-
-    public static final int MY_PERMISSIONS_REQUEST_CAMERA = 100;
-    public static final String ALLOW_KEY = "ALLOWED";
-    public static final String CAMERA_PREF = "camera_pref";
-
+    private StorageReference mStorageRef;
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        Log.d(TAG,"Working");
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Log.d(TAG,"Working");
-        setContentView(R.layout.camera_view);
-        Log.d(TAG,"Working");
-        this.imageView = (ImageView)this.findViewById(R.id.imageView1);
-        Button photoButton = (Button) this.findViewById(R.id.button1);
-        photoButton.setOnClickListener(new View.OnClickListener() {
+        setContentView(R.layout.activity_camera);
+        if (null == savedInstanceState) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.container, Camera2BasicFragment.newInstance())
+                    .commit();
+            //Log.d("TAG","WOOOOORRRRKK");
+        }
 
-            @Override
-            public void onClick(View v) {
-                Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
-                startActivityForResult(cameraIntent, CAMERA_REQUEST);
-            }
-        });
+        mStorageRef = FirebaseStorage.getInstance().getReference();
     }
 
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (requestCode == CAMERA_REQUEST && resultCode == Activity.RESULT_OK) {
-            Bitmap photo = (Bitmap) data.getExtras().get("data");
-            imageView.setImageBitmap(photo);
-        }
+
+    public void  UploadFile(Uri file){
+        final String uuid = UUID.randomUUID().toString().replace("-", "");
+        StorageReference riversRef = mStorageRef.child("images/"+uuid+".jpg");
+        Log.d("TAG","The file is " + file);
+        riversRef.putFile(file)
+                .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                        // Get a URL to the uploaded content
+                        Uri downloadUrl = taskSnapshot.getDownloadUrl();
+                        Log.d("TAG","Passed");
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception exception) {
+                        // Handle unsuccessful uploads
+                        // ...
+                        Log.d("TAG", "Failed");
+                        Log.e("TAG", "exception", exception);
+                    }
+                });
     }
 
 }
