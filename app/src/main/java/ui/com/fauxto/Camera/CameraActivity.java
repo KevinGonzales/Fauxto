d1316e9869549aec19ed40255e645fbb33e4f95c
Kevin Gonzales <KevinGonzalesVargas@gmail.com>
2018-03-09 20:27:32 -0600

    need to fix image resolution

--- app/src/main/java/ui/com/fauxto/Camera/CameraActivity.java
+++ app/src/main/java/ui/com/fauxto/Camera/CameraActivity.java
@@ -1,13 +1,47 @@
 package ui.com.fauxto.Camera;
 
+import android.Manifest;
 import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.content.FileProvider;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+
+import android.Manifest;
+import android.app.Activity;
+import android.app.AlertDialog;
+
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.Settings;
+
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 
 import ui.com.fauxto.R;
 
@@ -21,6 +55,11 @@ public class CameraActivity extends Activity {
 
     private static final int CAMERA_REQUEST = 1888;
     private ImageView imageView;
+
+    public static final int MY_PERMISSIONS_REQUEST_CAMERA = 100;
+    public static final String ALLOW_KEY = "ALLOWED";
+    public static final String CAMERA_PREF = "camera_pref";
+
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -47,4 +86,5 @@ public class CameraActivity extends Activity {
             imageView.setImageBitmap(photo);
         }
     }
+
 }
