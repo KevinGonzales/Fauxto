5a96270d147607ed41aee49b2222c6809398daf5
Myka Hancevic <bsx745@my.utsa.edu>
2018-03-21 15:19:08 -0500

    register activity

---
+++ app/src/main/java/ui/com/fauxto/Login/RegisterActivity.java
@@ -0,0 +1,29 @@
+package ui.com.fauxto.Login;
+
+import android.os.Bundle;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+
+import ui.com.fauxto.R;
+import ui.com.fauxto.UserFeed.UserFeedController;
+
+/**
+ * Created by myka on 3/7/18.
+ */
+
+public class RegisterActivity extends AppCompatActivity {
+    private static final String TAG = "RegisterActivity";
+
+   /* //firebase authentication
+    private FirebaseAuth mAuth;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.register_page);
+    }
+*/
+
+
+}
