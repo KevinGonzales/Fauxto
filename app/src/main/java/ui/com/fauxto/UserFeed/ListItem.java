3b677b5fef17a4b1a47ae89e31d5787f4e9def62
Kevin Gonzales <KevinGonzalesVargas@gmail.com>
2018-03-23 06:58:15 -0500

    DONE

--- app/src/main/java/ui/com/fauxto/UserFeed/ListItem.java
+++ app/src/main/java/ui/com/fauxto/UserFeed/ListItem.java
@@ -1,5 +1,11 @@
 package ui.com.fauxto.UserFeed;
 
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.os.Debug;
+import android.util.Log;
+import android.widget.ImageView;
+
 /**
  * Created by kevingonzales on 3/21/18.
  */
@@ -8,14 +14,22 @@ public class ListItem {
 
     private String head;
     private String desc;
+    private Bitmap image;
 
-    public ListItem(String head, String desc) {
+    public ListItem(String head, String desc,Bitmap im) {
         this.head = head;
         this.desc = desc;
+        this.image = im;
+        Log.d("boooo","In listItem" + im.toString());
     }
 
     public String getHead() {
         return head;
+    }
+
+    public Bitmap getImage() {
+
+        return image;
     }
 
     public String getDesc() {
--- app/src/main/java/ui/com/fauxto/UserFeed/MyAdapter.java
+++ app/src/main/java/ui/com/fauxto/UserFeed/MyAdapter.java
@@ -1,10 +1,13 @@
 package ui.com.fauxto.UserFeed;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.util.List;
@@ -37,24 +40,27 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
 
         holder.textViewHead.setText(listItem.getHead());
         holder.textViewDesc.setText(listItem.getDesc());
-
+        holder.imageView.setImageBitmap(listItem.getImage());
 
     }
+
 
     @Override
     public int getItemCount() {
         return listItems.size();
     }
 
+    //holder is the list item and its being set to the right things
     public class ViewHolder extends RecyclerView.ViewHolder
     {
 
         public TextView textViewHead;
         public TextView textViewDesc;
+        public ImageView imageView;
 
         public ViewHolder(View itemView) {
             super(itemView);
-
+            imageView = itemView.findViewById(R.id.image);//typo killed me!!!
             textViewHead = itemView.findViewById(R.id.textViewHead);
             textViewDesc = itemView.findViewById(R.id.description);
 
